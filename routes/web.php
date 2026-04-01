8da57d73fc9199ac9dd4cfd82dca6ab7d8462310
Maduks456 <madarsd41@gmail.com>
2026-04-01 14:59:25 +0300

    added some pages

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/why', function () {
+    return view('why');
+});
+Route::get('/iphone', function () {
+    return view('iphone');
+});
