639b1f46d7940101278e1c126bc8f092efd98c0b
Maduks456 <madarsd41@gmail.com>
2026-07-11 00:47:56 +0300

    bugfixing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,14 @@ return Application::configure(basePath: dirname(__DIR__))
    ->withMiddleware(function (Middleware $middleware) {
     $middleware->trustProxies(at: '*');
 })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+    ->withExceptions(function (Exceptions $exceptions) {
+    $exceptions->render(function (\Throwable $e, $request) {
+        return response(
+            "ERROR: " . $e->getMessage() . "\n" .
+            "FILE: " . $e->getFile() . " LINE: " . $e->getLine() . "\n\n" .
+            "TRACE:\n" . $e->getTraceAsString(),
+            500,
+            ['Content-Type' => 'text/plain']
+        );
+    });
+})->create();
