294befab3b08682cd39da6a04107328feabb2010
Maduks456 <madarsd41@gmail.com>
2026-07-10 22:32:19 +0300

    pretty

--- resources/views/iphone.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout>
-    <x-slot:title>
-        Iphone
-    </x-slot:title>
-    <h1>Iphone</h1>
-</x-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,19 @@
 
 @auth
 <x-layout>
-    <h1>Sveiks {{ Auth::user()->first_name}}</h1>
+    <h1>Hello {{ Auth::user()->first_name}}</h1>
 </x-layout>
 @endauth
 @guest
-<a href="/why">Kapēc</a>
-  <p>Sveiks, viesi!</p>
-  <a href="/login">login</a>
-  <a href="/register">Register</a>
+  <p>Hello, guest!</p>
+  <a href="/login">
+    <button>
+      Login
+    </button>
+  </a>
+  <a href="/register">
+    <button>
+      Register
+    </button>
+  </a>
 @endguest
