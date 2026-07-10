b9efc9fc8471ee17553ae3aaffcd22758f957f86
Maduks456 <madarsd41@gmail.com>
2026-07-10 23:06:46 +0300

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,30 @@
-<x-layout>
+
 @auth
-
+<x-layout>
     <h1>Hello {{ Auth::user()->first_name}}</h1>
-
+</x-layout>
 @endauth
 @guest
-  <p>Hello, guest!</p>
-  <a href="/login">
-    <button>
-      Login
-    </button>
-  </a>
-  <a href="/register">
-    <button>
-      Register
-    </button>
-  </a>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="{{ asset('style.css') }}">
+      <title>{{$title ?? "Uzdevumi un dienasgrāmata"}}</title>
+  </head>
+  <body>
+    <p>Hello, guest!</p>
+    <a href="/login">
+      <button>
+        Login
+      </button>
+    </a>
+    <a href="/register">
+      <button>
+        Register
+      </button>
+    </a>
+  </body>
+</html>
 @endguest
-</x-layout>
