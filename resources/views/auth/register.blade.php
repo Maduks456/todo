e18d017122b3290c5a74726904afbafbdcd37948
Maduks456 <madarsd41@gmail.com>
2026-07-17 17:51:20 +0300

    HOPE

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,28 +12,28 @@
             <h1>Register</h1>
         </div>
         <div class="box_logreg">
-            <div>
-                <form method="POST">
-                        @csrf
-                        @if ($errors->any())
-                                @foreach ($errors->all() as $error)
-                                    {{ $error }}
-                                @endforeach
-                        @endif
-            </div>
-            <div class="box_logreg_inputbox">
-                <label>Name: <input name="first_name" value="{{old('first_name')}}" required></label><br>
-                <label>Surname: <input name="last_name" value="{{old('last_name')}}"required></label><br>
-                <label>Email: <input type="email" name="email" value="{{old('email')}}"required></label><br>
-                <label>Password: <input type="password" name="password" required></label><br>
-                <label>Confirm Password: <input type="password" name="password_confirmation" required></label><br>
-            </div>
-            <div class="line"></div>
-            <div class="box_logreg_inputbox">
-                <button class=" input_button green">Register</button>
-                <p>Already have an account: <a href="/login">Login</a></p>
-            </div>
-                
+            <form method="POST">
+                <div>
+                    @csrf
+                    @if ($errors->any())
+                            @foreach ($errors->all() as $error)
+                                {{ $error }}
+                            @endforeach
+                    @endif
+                </div>
+                <div class="box_logreg_inputbox">
+                    <label>Name: <input name="first_name" value="{{old('first_name')}}" required></label><br>
+                    <label>Surname: <input name="last_name" value="{{old('last_name')}}"required></label><br>
+                    <label>Email: <input type="email" name="email" value="{{old('email')}}"required></label><br>
+                    <label>Password: <input type="password" name="password" required></label><br>
+                    <label>Confirm Password: <input type="password" name="password_confirmation" required></label><br>
+                </div>
+            
+                <div class="line"></div>
+                <div class="box_logreg_inputbox">
+                    <button class=" input_button green">Register</button>
+                    <p>Already have an account: <a href="/login">Login</a></p>
+                </div>
             </form>
         </div>
     </div>
