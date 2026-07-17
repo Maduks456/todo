a59e9e8b41a5a15f414e1dc4a01c2a50542b1234
Maduks456 <madarsd41@gmail.com>
2026-07-17 10:52:34 +0300

    clean up the code

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,12 +11,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-#[Fillable(['first_name','last_name','email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+    protected $fillable = ['first_name', 'last_name', 'email', 'password'];
+    protected $hidden = ['password', 'remember_token'];
 
     /**
      * Get the attributes that should be cast.
